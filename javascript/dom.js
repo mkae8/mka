0350941b0e3d470f0d28361c1bee0b63807c26c3
24LP9087 24LP9087 <24LP9087@G302-CC0115.local>
2024-07-22 12:33:16 +0800

    w

---
+++ javascript/dom.js
@@ -0,0 +1,52 @@
+// const a = document.getElementById("hello");
+// a.innerText = "Hello world";
+// a.style ="color:red"
+// console.log(a)
+
+// const bttnMinus = document.getElementsByClassName("bttnMinus")[0];
+
+// const bttnPlus = document.getElementsByClassName("bttnPlus")[0];
+
+// const input = document.getElementsByTagName("input")[0];
+
+// bttnPlus.addEventListener("click", () => {
+//   input.value = parseInt(input.value) + 1;
+// });
+
+// bttnMinus.addEventListener("click", () => {
+//   input.value = parseInt(input.value) - 1;
+// });
+
+// const inputOne = document.getElementById("inputOne");
+
+// const inputTwo = document.getElementById("inputTwo");
+
+// const result = document.getElementById("result");
+
+// const resultBttn = document.getElementById("resultBttn");
+
+// resultBttn.addEventListener("click", () => {
+//   result.innerText = parseInt(inputOne.value) * parseInt(inputTwo.value);
+// });
+
+
+// resultBttn.addEventListener("click", () => {
+//   const multi = inputOne.value * inputTwo.value;
+//   result.innerText = multi;
+//   console.log(multi);
+// });
+
+const inputOne = document.getElementById("inputOne");
+
+const inputTwo = document.getElementById("inputTwo");
+
+const result = document.getElementById("result");
+
+const resultBttn = document.getElementById("resultBttn");
+
+resultBttn.addEventListener("click", () =>{
+    switch() {
+        case 1:
+            inputOne.value * inputTwo.value; 
+    }
+})
--- javascript/objectBodlogo.js
+++ javascript/objectBodlogo.js
@@ -1,134 +1,89 @@
-// ------------> Bodlogo 1
-
-// Huvirgah  "2020 Toyota Coralla" gedeg shig
+// // Huvirgah  "2020 Toyota Coralla" gedeg shig
 // const movie = {
 //   title: "Inception",
 //   director: "Christopher Nolan",
 //   year: 2010,
 // };
-//  const result = `${movie.year}  ${movie.title} ${movie.director}`
-//  console.log(result);
-// console.log(`${movie.year}  ${movie.title} ${movie.director}`);
+// const printFullname = `${movie.year} ${movie.title} ${movie.director}`;
+// console.log(printFullname);
 
 // const album = {
 //   title: "Abbey Road",
 //   artist: "The Beatles",
-//   releaseYear: 1969
+//   releaseYear: 1969,
 // };
 
-// console.log(`${album.releaseYear} ${album.title} ${album.artist}`);
+// const printFullname = `${album.releaseYear} ${album.title} ${album.artist}`;
 
-// const album = {
+// console.log(printFullname);
+
+// const jackson = {
 //   title: "Thriller",
 //   artist: "Michael Jackson",
 //   releaseYear: 1982,
 //   genre: "Pop",
 // };
 
-// console.log(
-//   `${album.releaseYear} ${album.title} ${album.artist} ${album.genre}`
-// );
+// const printFullname = `${jackson.releaseYear} ${jackson.title} ${jackson.artist} ${jackson.genre}`;
+// console.log(printFullname);
 
-// Bodlogo 1
+// // Grade 60aas ih buyu tentsuu baih ueiin humuusiin neriig arrayd hadgal gedeg shig
+// // Salary 50000 buyu tentsuu
 
-// const sales = [
-//     { product: "Apple", quantity: 10 },
-//     { product: "Apple", quantity: 7 },
-//     { product: "Banana", quantity: 5 },
-//     { product: "Banana", quantity: 3 },
-//   ];
-
-//   const totalQuantities = sales.reduce((acc, { product, quantity }) => {
-//     if (acc[product]) {
-//       acc[product] += quantity;
-//     } else {
-//       acc[product] = quantity;
-//     }
-//     return acc;
-//   }, {});
-
-//   // Bodlogo 2
-//   const book = { title: "To Kill a Mockingbird", author: "Harper Lee", year: 1960 };
-
-//   const getValue = (key) => book[key];
-
-// -------------->> Bodlogo 2
-
-//   const students = [
-//     { name: "Bob", grade: 59 },
-//     { name: "Alice", grade: 85 },
-//     { name: "Charlie", grade: 72 },
-//   ];
-
-//   const names = students.filter((item) => item.grade >= 60).map(({ name }) => name);
-
-// Grade 60aas ih buyu tentsuu baih ueiin humuusiin neriig arrayd hadgal gedeg shig
-// Salary 50000 buyu tentsuu
 // const employees = [
 //   { name: "John", salary: 50000 },
 //   { name: "Jane", salary: 60000 },
 //   { name: "Jim", salary: 45000 },
 // ];
 
-// const workers = employees
+// const worker = employees
 //   .filter((item) => item.salary >= 50000)
 //   .map(({ name }) => name);
+//   console.log(worker);
 
-// console.log(workers);
+// // Price 1000 aas ihiig
 
-// Price 1000 aas ihiig
 // const products = [
 //   { name: "Laptop", price: 1200 },
 //   { name: "Phone", price: 800 },
 //   { name: "Tablet", price: 400 },
 // ];
-// const electricProducts = products
+
+// const electricPro = products
 //   .filter((item) => item.price > 1000)
 //   .map(({ name }) => name);
-// console.log(electricProducts);
+// console.log(electricPro);
 
-// title ni type ni number baival ter objectiig ol
+// // title ni type ni number baival ter objectiig ol
+
 // const books = [
 //   { title: 1984, author: "George Orwell" },
 //   { title: "Brave New World", author: "Aldous Huxley" },
 //   { title: "Fahrenheit 451", author: "Ray Bradbury" },
 // ];
 
-// const getTitleNumber = books.find(({ title }) => typeof title === "number");
+// const numberTitle = books.find((item) => typeof item.title === "number");
+// console.log(numberTitle);
 
-// console.log(getTitleNumber);
+// // Price 10 aas ih baigab baraanii nerig hevle. Hervee baihgui bol not found gej hevle. Ternary operator ashiglana gedeg bodlogo shig
+// // Price 150 aas ih iig
 
-// ------------>> Bodlogo 3
-
-//   const products = [
-//     { name: "Pen", price: 5 },
-//     { name: "Eraser", price: 1 },
-//     { name: "Notebook", price: 12 },
-//   ];
-
-//   const getProducts = () => {
-//     const matchedProducts = products.filter(({ price }) => price > 10).map(({ name }) => name);
-
-//     return matchedProducts.length ? matchedProducts : "Not found";
-//   };
-
-// Price 10 aas ih baigab baraanii nerig hevle. Hervee baihgui bol not found gej hevle. Ternary operator ashiglana gedeg bodlogo shig
-
-// Price 150 aas ih iig
 // const badgets = [
-//   { name: "Smartphone", price: 699 },
-//   { name: "Smartwatch", price: 199 },
+//   { name: "Smartphone", price: 619 },
+//   { name: "Smartwatch", price: 19 },
 //   { name: "Bluetooth Speaker", price: 49 },
 // ];
-// const getBatgets = () => {
+
+// const getBadgets = () => {
 //   const greaterThan = badgets
 //     .filter((item) => item.price > 150)
 //     .map(({ name }) => name);
 //   return greaterThan.length ? greaterThan : "Not found";
 // };
-// console.log(getBatgets());
+// console.log(getBadgets());
 
-// Price 2 ih buyu tentsuu
+// // Price 2 ih buyu tentsuu
+
 // const groceries = [
 //   { name: "Apple", price: 1 },
 //   { name: "Milk", price: 3 },
@@ -136,29 +91,31 @@
 // ];
 
 // const getGroceries = () => {
-//   const foods = groceries
-//     .filter((item) => item.price >= 2)
+//   const greaterThan = groceries
+//     .filter((item) => item.price >= 4)
 //     .map(({ name }) => name);
-//   return foods.length ? foods : "Not found";
+//   return greaterThan.length ? greaterThan : "Not found";
 // };
+
 // console.log(getGroceries());
 
-// Price 100 as ih
+// // Price 100 as ih
+
 // const furniture = [
 //   { name: "Chair", price: 45 },
 //   { name: "Table", price: 150 },
 //   { name: "Lamp", price: 25 },
 // ];
 
-// const getFurniture = () => {
+// const getTavilga = () => {
 //   const tavilga = furniture
-//     .filter((item) => item.price > 1000)
+//     .filter((item) => item.price > 100)
 //     .map(({ name }) => name);
-//   return tavilga.length ? tavilga : " bhq bnaa ";
+//   return tavilga.length ? tavilga : "Not found ";
 // };
-// console.log(getFurniture());
+// console.log(getTavilga());
 
-// ----------->> Bodlogo 4
+// // Hun tus buriin  ovog neriig ashiglaad fullname iig ni hevle gedeg shig
 
 // const authors = [
 //   { firstName: "Mark", lastName: "Twain" },
@@ -166,9 +123,8 @@
 //   { firstName: "Jane", lastName: "Austen" },
 // ];
 
-// const printFullName  = authors.map(({firstName, lastName})=> `${firstName} ${lastName}`)
-
-// console.log(printFullName);
+// const fullname = authors.map(({firstName,lastName})=> `${firstName} ${lastName}`)
+// console.log(fullname);
 
 // const students = [
 //   { firstName: "Alice", lastName: "Johnson" },
@@ -176,10 +132,8 @@
 //   { firstName: "Carol", lastName: "Davis" },
 // ];
 
-// const printFullName = students.map(
-//   ({ firstName, lastName }) => `${firstName} ${lastName}`
-// );
-// console.log(printFullName);
+// const fullname = students.map(({firstName,lastName})=> `${firstName} ${lastName}`)
+// console.log(fullname);
 
 // const musicians = [
 //   { firstName: "Ludwig", lastName: "Beethoven" },
@@ -187,42 +141,23 @@
 //   { firstName: "Johann", lastName: "Bach" },
 // ];
 
-// const printFullNmae = musicians.map(
+// const fullname = musicians.map(
 //   ({ firstName, lastName }) => `${firstName} ${lastName}`
 // );
-// console.log(printFullNmae);
+// console.log(fullname);
 
-//   const people = [
-//     { firstName: "John", lastName: "Doe" },
-//     { firstName: "Jane", lastName: "Smith" },
-//     { firstName: "Emily", lastName: "Jones" },
-//   ];
-
-//   const fullnames = people.map(({ firstName, lastName }) => `${firstName} ${lastName}`);
-
-// ----------> Bodlogo 5
-
-//   const books = [
-//     { title: "1984", year: 1949 },
-//     { title: "The Great Gatsby", year: 1925 },
-//     { title: "To Kill a Mockingbird", year: 1960 },
-//   ];
-
-//   const updatedBooks = books.map((book) => ({writtenYear: new Date().getFullYear() - book.year,...book,
-//   }));
-
+// // Zohiol tus buriig hden jiliin omno zohiogdoj baisniig ni hevle. Hugatsaag ni haruulsan shine keyless update hiine gedeg shig
 // const movies = [
 //   { title: "The Shawshank Redemption", year: 1994 },
 //   { title: "The Godfather", year: 1972 },
 //   { title: "The Dark Knight", year: 2008 },
 // ];
 
-// const updatedMovies = movies.map((item) => ({
-//   writtenYear: new Date().getFullYear() - item.year,
-//   ...item,
+// const year = movies.map((movie) => ({
+//   writtenYear: new Date().getFullYear() - movie.year,
+//   ...movie,
 // }));
-
-// console.log(updatedMovies);
+// console.log(year);
 
 // const albums = [
 //   { title: "Abbey Road", year: 1969 },
@@ -230,11 +165,11 @@
 //   { title: "Thriller", year: 1982 },
 // ];
 
-// const updatedAlbums = albums.map((item) => ({
-//   writtenYear: new Date().getFullYear() - item.year,
-//   ...item,
+// const year = albums.map((album) => ({
+//   writtenYear: new Date().getFullYear() - album.year,
+//   ...album,
 // }));
-// console.log(updatedAlbums);
+// console.log(year);
 
 // const plays = [
 //   { title: "Hamlet", year: 1603 },
@@ -242,57 +177,39 @@
 //   { title: "Death of a Salesman", year: 1949 },
 // ];
 
-// const updatedPlays = plays.map((item) => ({
-//   writtenYear: new Date().getFullYear() - item.year,
-//   ...item,
+// const year = plays.map((play) => ({
+//   writtenYear: new Date().getFullYear() - play.year,
+//   ...play,
 // }));
+// console.log( year);
 
-// console.log(updatedPlays);
-
-// ----------> Bodlogo 6
-// Duriin id aar ni completed iig update hiih function bich
-
-//   const tasks = [
-//     { id: 1, description: "Do laundry", completed: false },
-//     { id: 2, description: "Clean dishes", completed: false },
-//     { id: 3, description: "Buy groceries", completed: false },
-//   ];
-
-//   const updateTask = (taskId) => {
-//     const foundTask = tasks.find(({ id }) => id === taskId);
-
-//     if (!foundTask) return "Not found";
-
-//     foundTask.completed = true;
-
-//     return tasks;
-//   };
-
-//   console.log(updateTask(2));
+// // Duriin id aar ni completed iig update hiih function bich
 
 // const events = [
 //   { id: 1, name: "Conference", attended: true },
 //   { id: 2, name: "Workshop", attended: false },
 //   { id: 3, name: "Meetup", attended: false },
 // ];
-// const updatedEvent = (eventsId) => {
-//   const foundEvent = events.find(({ id }) => id === eventsId);
-//   if (!foundEvent) return "Id bhq bnaa";
-//   foundEvent.attended = true;
-//   return events;
-// };
-// console.log(updatedEvent());
 
-// const appointments = [
-//   { id: 1, description: "Dentist appointment", attended: true },
-//   { id: 2, description: "Team meeting", attended: false },
-//   { id: 3, description: "Lunch with a friend", attended: false },
-// ];
-
-// const updatedAppoint = (appointmentsId) => {
-//   const findAppoint = appointments.find(({ id }) => id === appointmentsId);
-//   if (!findAppoint) return "Not found";
-//   findAppoint.attended = true;
-//   return appointments;
+// const updateEvent = (taskId) => {
+//   const updated = events.find(({ id }) => id === taskId);
+//   if (!updated) return "Not found";
+//   updated.attended = true;
+//   return events
 // };
-// console.log(updatedAppoint(2));
+// console.log(updateEvent(2));
+
+const appoint = [
+  { id: 1, description: "Dentist appointment", attended: true },
+  { id: 2, description: "Team meeting", attended: false },
+  { id: 3, description: "Lunch with a friend", attended: false },
+];
+
+const updated = (itemId) => {
+  const task = appoint.find(({ id }) => itemId === id);
+  if (!task) return "Not found";
+  task.attended = true;
+  return appoint;
+};
+
+console.log(updated());
